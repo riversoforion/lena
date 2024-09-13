3bb8008016dd70f9d3f6fe066a4319918a5ef6c0
Eric McIntyre <mac@riversoforion.com>
2024-09-13 06:54:47 -0700

    build: Add publishing tasks
    For now, tasks only publish to a project-local or system-local
    repository. PGP signing is enabled.
    
    Refs: #1

--- buildSrc/src/main/kotlin/lena.conventions.gradle.kts
+++ buildSrc/src/main/kotlin/lena.conventions.gradle.kts
@@ -4,24 +4,74 @@
 
 plugins {
     `java-library`
+    `maven-publish`
+    signing
     // semver
 }
 
-group = "org.riversoforion.lena"
+group = "com.riversoforion.lena"
 version = "0.1.0"
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
 java {
     toolchain {
-        version = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(21)
+        withJavadocJar()
+        withSourcesJar()
     }
+}
+
+tasks.javadoc {
+    (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
 }
 
 tasks.named<Test>("test") {
     // -XX:+EnableDynamicAgentLoading : For ByteBuddy, used by Mockito/SystemStubs
     jvmArgs("-XX:+EnableDynamicAgentLoading")
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("lena") {
+            from(components["java"])
+
+            pom {
+                url = "https://github.com/riversoforion/lena"
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "macdaddyaz"
+                        name = "Eric McIntyre"
+                        email = "mac@riversoforion.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/riversoforion/lena.git"
+                    developerConnection = "scm:git:ssh://github.com/riversoforion/lena.git"
+                    url = "https://github.com/riversoforion/lena"
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "local"
+            url = uri(layout.buildDirectory.dir("repo"))
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["lena"])
 }
